e5ce4496e0491097ffe9e92df345f11f48a34d74
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2024-06-24 12:18:26 +0800

    Added images
    Added images to be included in the manuscript. Updated the website

--- system/streamlit/main.py
+++ system/streamlit/main.py
@@ -22,6 +22,8 @@ df = df.rename(columns={'Latitude': 'LAT', 'Longitude': 'LON'})
 df = df.dropna()
 
 st.write("## System Description")
+text = "iHOPE is a hybrid optimization and prioritization system that looks for new locations to put up rural health units in the Philippines."
+st.write(text)
 st.write("## System Components:")
 # Display the DataFrame
 st.write("### Existing RHUs:")
@@ -38,28 +40,28 @@ regions = [
 selected_region = st.selectbox("Select a Region", regions)
 st.write(f"You selected: {selected_region}")
 st.write("## Display Candidate Sites")
-cs_df = pd.read_csv('../../CandidateSites/rg1_candidate_sites.csv')
-cs_df['ID'] = cs_df['ID'].astype(int)
-st.write(cs_df)
-st.write(hexagons)
+if selected_region == "Region I":
+    cs_df = pd.read_csv('../../CandidateSites/rg1_candidate_sites.csv')
+    cs_df['ID'] = cs_df['ID'].astype(int)
+    st.write(cs_df)
+    # st.write(hexagons)
 
 st.write("## Display Optimal Sites")
 num_optimal_sites = int(st.text_input("How many optimal sites would you want?", 1))
 st.write("You entered:", num_optimal_sites)
+if selected_region == "Region I":
+    raw_rg1_clustered = pd.read_csv("../../CandidateSites/rg1_PCF_data.csv") # for finding optimal locations
+    neighbors_df = pd.read_csv('../../CandidateSites/rg1_Neighbors.csv')
+    candidate_sites = pd.read_csv("../../CandidateSites/rg1_candidate_sites.csv")
+    neighbors_df.rename(columns={'fid': 'ID'}, inplace=True)
+    raw_rg1_clustered = raw_rg1_clustered.drop(columns=['Neighbors'])
 
-raw_rg1_clustered = pd.read_csv("../../CandidateSites/rg1_PCF_data.csv") # for finding optimal locations
-neighbors_df = pd.read_csv('../../CandidateSites/rg1_Neighbors.csv')
-candidate_sites = pd.read_csv("../../CandidateSites/rg1_candidate_sites.csv")
-neighbors_df.rename(columns={'fid': 'ID'}, inplace=True)
-raw_rg1_clustered = raw_rg1_clustered.drop(columns=['Neighbors'])
-
-selected_facilities_df, hex_with_RHU, og_HCFAI, updated_HCFAI = optimal.optimize(raw_rg1_clustered, candidate_sites, neighbors_df, num_optimal_sites) # 81 for dropping non buildable; 363 for validation)
-selected_facilities = list(selected_facilities_df['ID'])
-st.write(f"Original HCFAI: {og_HCFAI}")
-st.write(f"Updated HCFAI: {updated_HCFAI}")
-st.write(selected_facilities_df[['ID', 'RHU_Presence', 'HCFAI']])
-# for site in selected_facilities:
-#     st.write(site)
+    selected_facilities_df, hex_with_RHU, og_HCFAI, updated_HCFAI = optimal.optimize(raw_rg1_clustered, candidate_sites, neighbors_df, num_optimal_sites) # 81 for dropping non buildable; 363 for validation)
+    selected_facilities = list(selected_facilities_df['ID'])
+    st.write(f"Original HCFAI: {og_HCFAI}")
+    st.write(f"Updated HCFAI: {updated_HCFAI}")
+    st.write(selected_facilities_df[['ID', 'RHU_Presence', 'HCFAI']])
+    st.write("""<div style="width:100%;text-align:center;"><a href="https://matereyes00.github.io/iHOPE/rg1_map/"</a>Region 1</div>""", unsafe_allow_html=True)
 
 st.write("## Display Prioritized Sites")
 num_prioritized_sites = st.text_input("How many RHUs do you want to put up?", "")
