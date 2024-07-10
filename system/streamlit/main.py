fe8f00e9d1a5a8c48efed81d1783609b78da23ec
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2024-07-10 17:09:35 +0800

    Added raw path file to every csv file called

--- system/streamlit/main.py
+++ system/streamlit/main.py
@@ -62,32 +62,43 @@ st.write("Please input a number to begin the optimization process. You can only 
 num_optimal_sites = int(st.text_input("How many optimal sites would you want?", 1))
 st.write("You entered:", num_optimal_sites)
 if selected_region == "Region I":
-    raw_rg1_clustered = pd.read_csv("rg1_PCF.csv") # for finding optimal locations
-    neighbors_df = pd.read_csv('rg1_Neighbors.csv')
-    candidate_sites = pd.read_csv("rg1candidate_sites.csv")
-    neighbors_df.rename(columns={'fid': 'ID'}, inplace=True)
-    # raw_rg1_clustered = rg1_clustered_df
-    raw_rg1_clustered = raw_rg1_clustered.drop(columns=['Neighbors'])
-    # selected_facilities_df, hex_with_RHU, og_HCFAI, updated_HCFAI = optimal.optimize(raw_rg1_clustered, candidate_sites, neighbors_df, num_optimal_sites) 
-    selected_facilities_df, hex_with_RHU, og_HCFAI, updated_HCFAI = optimal.optimize(raw_rg1_clustered, candidate_sites, neighbors_df, num_optimal_sites)
-    selected_facilities = list(set(selected_facilities_df['ID']))
-    st.write(f"Original HCFAI: {og_HCFAI}")
-    st.write(f"Updated HCFAI: {updated_HCFAI}")
-    st.write(selected_facilities_df[['ID', 'RHU_Presence', 'HCFAI']])
-    st.write("""<div style="width:100%;text-align:center;"><a href="https://matereyes00.github.io/iHOPE/rg1map/"</a>Region 1</div>""", unsafe_allow_html=True)
+    PCF_df= 'https://raw.githubusercontent.com/matereyes00/iHOPE/iHOPE_ver1.0/system/streamlit/rg1_PCF.csv'
+    NEIGHBOURS_df= 'https://raw.githubusercontent.com/matereyes00/iHOPE/iHOPE_ver1.0/system/streamlit/rg1_Neighbors.csv'
+    CS_df='https://raw.githubusercontent.com/matereyes00/iHOPE/iHOPE_ver1.0/system/streamlit/rg1candidate_sites.csv'
 
-st.write("## Cities needing RHUs: ")
-st.write("Based on the number of optimal sites you specified above, this section automatically generates the number of RHUs needed in the cities")
-os = list(set(selected_facilities_df['ID']))
+    PCF_response=requests.get(PCF_df)
+    NEIGHBOURS_response=requests.get(NEIGHBOURS_df)
+    CS_response=requests.get(CS_df)
 
-x = "rg1-clusters.csv"
-rg1_clustered_df = pd.read_csv(x)
-os_df, rhus_per_city = optimal.MCLP(os, rg1_clustered_df)
-# st.write(rhus_per_city)
-for key, value in rhus_per_city.items():
-    if value:
-        st.write(f"{key}: {value}")
+    if PCF_response.status_code == 200 and NEIGHBOURS_response.status_code == 200 and CS_response.status_code == 200:
+        raw_rg1_clustered = pd.read_csv(StringIO(PCF_response.text)) # for finding optimal locations
+        neighbors_df = pd.read_csv(StringIO(NEIGHBOURS_response.text))
+        candidate_sites = pd.read_csv(StringIO(CS_response.text))
+        neighbors_df.rename(columns={'fid': 'ID'}, inplace=True)
+        # raw_rg1_clustered = rg1_clustered_df
+        raw_rg1_clustered = raw_rg1_clustered.drop(columns=['Neighbors'])
+        # selected_facilities_df, hex_with_RHU, og_HCFAI, updated_HCFAI = optimal.optimize(raw_rg1_clustered, candidate_sites, neighbors_df, num_optimal_sites) 
+        selected_facilities_df, hex_with_RHU, og_HCFAI, updated_HCFAI = optimal.optimize(raw_rg1_clustered, candidate_sites, neighbors_df, num_optimal_sites)
+        selected_facilities = list(set(selected_facilities_df['ID']))
+        st.write(f"Original HCFAI: {og_HCFAI}")
+        st.write(f"Updated HCFAI: {updated_HCFAI}")
+        st.write(selected_facilities_df[['ID', 'RHU_Presence', 'HCFAI']])
+        st.write("""<div style="width:100%;text-align:center;"><a href="https://matereyes00.github.io/iHOPE/rg1map/"</a>Region 1</div>""", unsafe_allow_html=True)
 
-st.write("## Display Prioritized Sites")
-st.write("Based on the number of optimal sites you specified above, this section automatically generates the areas that need RHUs, denoted by a hexagon ID.")
-recommend.recommend(os_df, rhus_per_city)
+        st.write("## Cities needing RHUs: ")
+        st.write("Based on the number of optimal sites you specified above, this section automatically generates the number of RHUs needed in the cities")
+        os = list(set(selected_facilities_df['ID']))
+
+        rg1_clusters_df = "https://raw.githubusercontent.com/matereyes00/iHOPE/iHOPE_ver1.0/system/streamlit/rg1-clusters.csv"
+        rg1_clusters_df_response=requests.get(rg1_clusters_df)
+        if rg1_clusters_df_response.status_code == 200:
+            rg1_clustered_df = pd.read_csv(StringIO(rg1_clusters_df_response.text))
+            os_df, rhus_per_city = optimal.MCLP(os, rg1_clustered_df)
+            # st.write(rhus_per_city)
+            for key, value in rhus_per_city.items():
+                if value:
+                    st.write(f"{key}: {value}")
+
+            st.write("## Display Prioritized Sites")
+            st.write("Based on the number of optimal sites you specified above, this section automatically generates the areas that need RHUs, denoted by a hexagon ID.")
+            recommend.recommend(os_df, rhus_per_city)
