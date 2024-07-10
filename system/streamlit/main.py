14ab1d5af82b32bf7a6835823060f1897ea4c56a
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2024-07-10 14:11:31 +0800

    test

--- system/streamlit/main.py
+++ system/streamlit/main.py
@@ -2,21 +2,18 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 import os
-# import geopandas as gpd
-# import folium
-# from streamlit_folium import st_folium
 import optimal
 import recommend
-# from utils import wrangle
 import path 
 import sys
 import pickle
 
 
 st.title("I-HOPE")
+
+# List all files in the current directory
 current_directory = os.getcwd()
 file_list = os.listdir(current_directory)
-st.write("Files in Current Directory:")
 existing_rhus_df = 'health_facilities_with_coordinates.csv'
 if existing_rhus_df in file_list:
     df = pd.read_csv(existing_rhus_df)
@@ -24,6 +21,8 @@ if existing_rhus_df in file_list:
     df = df[selected_columns]
     df = df.rename(columns={'Latitude': 'LAT', 'Longitude': 'LON'})
     df = df.dropna()
+    # st.write(df)
+
     st.write("## System Description")
     text = "iHOPE is a hybrid optimization and prioritization system that looks for new locations to put up rural health units in the Philippines. The user gets to select the region of their choice to select the number of RHUs to put up."
     st.write(text)
@@ -31,6 +30,20 @@ if existing_rhus_df in file_list:
     st.write("### Existing RHUs:")
     st.write(df)
     st.map(df)
+
+# df = pd.read_csv(existing_rhus_df)
+# selected_columns = ['Facility Name', 'Region Name', 'Latitude', 'Longitude']
+# df = df[selected_columns]
+# df = df.rename(columns={'Latitude': 'LAT', 'Longitude': 'LON'})
+# df = df.dropna()
+
+# st.write("## System Description")
+# text = "iHOPE is a hybrid optimization and prioritization system that looks for new locations to put up rural health units in the Philippines. The user gets to select the region of their choice to select the number of RHUs to put up."
+# st.write(text)
+# st.write("## System Components:")
+# st.write("### Existing RHUs:")
+# st.write(df)
+# st.map(df)
 
 # Define the list of regions
 regions = [
@@ -46,45 +59,42 @@ st.write("Select a region to see where you want to put up rural health units")
 if selected_region == "Region I":
     # execute BPNN application here
     # call model
-    if 'rg1candidate_sites.csv' in file_list:
-        cs_df = pd.read_csv('rg1candidate_sites.csv')
-        st.write(f"#### {len(cs_df['ID'])} Candidate Sites")
-        cs_df['ID'] = cs_df['ID'].astype(int)
-        st.write(cs_df)
+    cs_df = pd.read_csv('rg1candidate_sites.csv')
+    st.write(f"#### {len(cs_df['ID'])} Candidate Sites")
+    cs_df['ID'] = cs_df['ID'].astype(int)
+    st.write(cs_df)
 
 st.write("## Display Optimal Sites")
 st.write("Please input a number to begin the optimization process. You can only put a number less than or equal to the number of candidate sites specified above")
 num_optimal_sites = int(st.text_input("How many optimal sites would you want?", 1))
 st.write("You entered:", num_optimal_sites)
 if selected_region == "Region I":
-    if "rg1_PCF.csv" and 'rg1_Neighbors.csv' in file_list:
-        raw_rg1_clustered = pd.read_csv("rg1_PCF.csv") # for finding optimal locations
-        neighbors_df = pd.read_csv('rg1_Neighbors.csv')
-        # candidate_sites = pd.read_csv("rg1candidate_sites.csv")
-        neighbors_df.rename(columns={'fid': 'ID'}, inplace=True)
-        # raw_rg1_clustered = rg1_clustered_df
-        raw_rg1_clustered = raw_rg1_clustered.drop(columns=['Neighbors'])
-        # selected_facilities_df, hex_with_RHU, og_HCFAI, updated_HCFAI = optimal.optimize(raw_rg1_clustered, candidate_sites, neighbors_df, num_optimal_sites) 
-        selected_facilities_df, hex_with_RHU, og_HCFAI, updated_HCFAI = optimal.optimize(raw_rg1_clustered, cs_df, neighbors_df, num_optimal_sites)
-        selected_facilities = list(set(selected_facilities_df['ID']))
-        st.write(f"Original HCFAI: {og_HCFAI}")
-        st.write(f"Updated HCFAI: {updated_HCFAI}")
-        st.write(selected_facilities_df[['ID', 'RHU_Presence', 'HCFAI']])
-        st.write("""<div style="width:100%;text-align:center;"><a href="https://matereyes00.github.io/iHOPE/rg1map/"</a>Region 1</div>""", unsafe_allow_html=True)
+    raw_rg1_clustered = pd.read_csv("rg1_PCF.csv") # for finding optimal locations
+    neighbors_df = pd.read_csv('rg1_Neighbors.csv')
+    candidate_sites = pd.read_csv("rg1candidate_sites.csv")
+    neighbors_df.rename(columns={'fid': 'ID'}, inplace=True)
+    # raw_rg1_clustered = rg1_clustered_df
+    raw_rg1_clustered = raw_rg1_clustered.drop(columns=['Neighbors'])
+    # selected_facilities_df, hex_with_RHU, og_HCFAI, updated_HCFAI = optimal.optimize(raw_rg1_clustered, candidate_sites, neighbors_df, num_optimal_sites) 
+    selected_facilities_df, hex_with_RHU, og_HCFAI, updated_HCFAI = optimal.optimize(raw_rg1_clustered, candidate_sites, neighbors_df, num_optimal_sites)
+    selected_facilities = list(set(selected_facilities_df['ID']))
+    st.write(f"Original HCFAI: {og_HCFAI}")
+    st.write(f"Updated HCFAI: {updated_HCFAI}")
+    st.write(selected_facilities_df[['ID', 'RHU_Presence', 'HCFAI']])
+    st.write("""<div style="width:100%;text-align:center;"><a href="https://matereyes00.github.io/iHOPE/rg1map/"</a>Region 1</div>""", unsafe_allow_html=True)
 
-        st.write("## Cities needing RHUs: ")
-        st.write("Based on the number of optimal sites you specified above, this section automatically generates the number of RHUs needed in the cities")
-        os = list(set(selected_facilities_df['ID']))
+st.write("## Cities needing RHUs: ")
+st.write("Based on the number of optimal sites you specified above, this section automatically generates the number of RHUs needed in the cities")
+os = list(set(selected_facilities_df['ID']))
 
-        x = "rg1-clusters.csv"
-        if x in file_list:
-            rg1_clustered_df = pd.read_csv(x)
-            os_df, rhus_per_city = optimal.MCLP(os, rg1_clustered_df)
-            # st.write(rhus_per_city)
-            for key, value in rhus_per_city.items():
-                if value:
-                    st.write(f"{key}: {value}")
+x = "rg1-clusters.csv"
+rg1_clustered_df = pd.read_csv(x)
+os_df, rhus_per_city = optimal.MCLP(os, rg1_clustered_df)
+# st.write(rhus_per_city)
+for key, value in rhus_per_city.items():
+    if value:
+        st.write(f"{key}: {value}")
 
-            st.write("## Display Prioritized Sites")
-            st.write("Based on the number of optimal sites you specified above, this section automatically generates the areas that need RHUs, denoted by a hexagon ID.")
-            recommend.recommend(os_df, rhus_per_city)
+st.write("## Display Prioritized Sites")
+st.write("Based on the number of optimal sites you specified above, this section automatically generates the areas that need RHUs, denoted by a hexagon ID.")
+recommend.recommend(os_df, rhus_per_city)
